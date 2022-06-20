242a67fb7865c9f67465d07a3ccc41224a9b5c26
jesus jimenez <jimenezjesus@unbosque.edu.co>
2022-06-20 13:11:28 -0500

    feat: swagger added

---
+++ src/main/java/com/meli/dna/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.meli.dna.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.meli.dna.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo())
+                ;
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Dna Meli Service",
+                "How to know if dna is mutant or not",
+                "1.0",
+                "",
+                new Contact("Jesus Jimenez", "", "jesus.950810@gmail.com"),
+                "V1",
+                "V1",
+                Collections.emptyList()
+        );
+    }
+}
